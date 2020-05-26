71e3d5cba68441dd328aba4218b477df75fb1161
Lucas Rodrigues Freire <lucas-rodrigues23@hotmail.com>
2020-05-26 11:32:26 -0300

    Adicionando configurações básicas de rotas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from "@angular/router";
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from "./navbar/navbar.component";
 import { CasoscovidComponent } from "./casoscovid/casoscovid.component";
 import { CasocovidDetalhesComponent } from "./casoscovid/casocovid-detalhes/casocovid-datalhes.component";
+
+const ROUTES = RouterModule.forRoot([
+  {
+    path: 'casoscovid',
+    component: CasoscovidComponent
+  }
+])
 
 @NgModule({
   declarations: [
@@ -18,7 +26,8 @@ import { CasocovidDetalhesComponent } from "./casoscovid/casocovid-detalhes/caso
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    ROUTES
   ],
   providers: [],
   bootstrap: [AppComponent]
